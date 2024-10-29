033e6cbad471238385846adb084880106f1df4db
qhumphreys <146850641+qhumphreys@users.noreply.github.com>
2024-10-29 10:45:58 -0400

    Update script.js

--- script.js
+++ script.js
@@ -5,8 +5,13 @@ const printPage = () => {
     printFrame.style.display = 'none';
     printFrame.src = 'index.html';
     document.body.appendChild(printframe);
-    printFrame.contentWindow.focus();
-    printFrame.contentWindow.print();
+
+    var printWindow = window.open('', '_blank', 'height=8in,width=5.5in');
+    printWindow.document.write(printFrame.document.documentElement.outerHTML);
+    printWindow.document.close();
+    printWindow.focus();
+    printWindow.print();
+    printWindow.close();
 };
 
 printButton.addEventListener('click', () => {
